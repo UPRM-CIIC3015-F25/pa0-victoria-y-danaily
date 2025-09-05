e6e4594cf74f520bdc252c1242777a66eb4f02b9
victoria.toro <victoria.toro1@upr.edu>
2025-09-05 18:29:42 -0400

    task 5 ; conflict & merge

--- Game.py
+++ Game.py
@@ -12,9 +12,9 @@ def ball_movement():
 
     # Start the ball movement when the game begins
     # TODO Task 5 Create a Merge Conflict
-    speed = 7
+    speed = 10
     if start:
-        ball_speed_x = speed * random.choice((7, -5))  # Randomize initial horizontal direction
+        ball_speed_x =  * random.choice((1, -1))  # Randomize initial horizontal direction
         ball_speed_y = speed * random.choice((1, -1))  # Randomize initial vertical direction
         start = False
 
